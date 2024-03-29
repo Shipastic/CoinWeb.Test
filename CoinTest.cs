1857e78e3ed3b4455739d3ddc0ab71e4fb4ebd0c
Denis Shipelov <denkul87@mail.ru>
2024-03-29 11:56:12 +0300

    Initial commit

---
+++ CoinTest.cs
@@ -0,0 +1,36 @@
+﻿using CoinWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinWeb.Test
+{
+    public class CoinTest
+    {
+        [Fact]
+        public void ChangeCountry()
+        {
+            //Arrage
+            Coin coin = new Coin() { Id = 1, Country = "Italy", Year = 1973, Metal = "Aluminum", Face = "/images/img1.jpg", Denomination = "5   lire" };
+
+            
+
+            //Assert
+            Assert.Equal(1973, coin.Year);
+        }
+
+        [Fact]
+        public void RangeCountry()
+        {
+            //Arrage
+            Coin coin = new Coin() { Id = 1, Country = "Italy", Year = 1973, Metal = "Aluminum", Face = "/images/img1.jpg", Denomination = "5   lire" };
+
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => coin.Year = 2030);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => coin.Year = -100);
+        }
+    }
+}
